5c79fb0002b6e17ad93195a841231bcbc0d59be0
Jamshid Elmi <dariush1231@gmail.com>
2017-10-25 23:05:29 +0430

    fix emp profile

--- views/employees/emp_profile.php
+++ views/employees/emp_profile.php
@@ -1,6 +1,6 @@
 <div class="box box-primary box-solid">
     <div class="box-header with-border">
-        <h3 class="box-title">اطلاعات کارمند: <?=$employee->emp_name ." ". $employee->emp_lname ?></h3>
+        <h3 class="box-title">اطلاعات کارمند: <?php echo ($employee->emp_gendar == 0) ? 'خانم' : 'آقای'; ?> <?=$employee->emp_name ." ". $employee->emp_lname ?></h3>
         <div class="pull-left box-tools no-print">
             <a target="_BLANK" class="btn btn-info btn-sm" href="<?php echo site_url('employee/print_profile/'.$employee->emp_id); ?>" data-toggle="tooltip" title="" data-original-title="Print"> <i class="fa fa-print fa-lg"></i></a>
         </div>
@@ -8,8 +8,6 @@
     </div>
     <!-- /.box-header -->
     <div class="box-body">
-        <?php // print_r($employee); ?>
-        <?php // print_r($users); ?>
         <div class="row">
             <div class="col-xs-3">
                 <img width="150" src="<?=base_url('assets/img/profiles/'.$employee->emp_picture); ?>" class="img-thumbnail" alt="">
@@ -74,14 +72,32 @@
                                 <td><?=mds_date("l Y/F/d ", strtotime('+1 years', $employee->emp_join_date), 0); ?></td>
                             </tr>
                             <tr>
-                                <th>تخلص: </th>
-                                <td><?=$employee->emp_lname ?>  </td>
-                                <th>پست: </th>
-                                <td><?=($employee->emp_position) ?>  </td>
-                                <th>تاریخ ختم قرارداد</th>
-                                <td><?=mds_date("l Y/F/d ", strtotime('+1 years', $employee->emp_join_date), 0); ?></td>
+                                <th>شماره تذکره: </th>
+                                <td><?=$employee->emp_national_id ?>  </td>
+                                <th>شماره تماس: </th>
+                                <td><?=($employee->emp_phone) ?>  </td>
+                                <th>ایمیل آدرس:</th>
+                                <td><?=$employee->emp_email ?></td>
+                            </tr>
+                            <tr>
+                                <th>سکونت اصلی: </th>
+                                <td><?=$employee->emp_org_place ?>  </td>
+                                <th>سکونت فعلی: </th>
+                                <td><?=($employee->emp_cur_place) ?>  </td>
+                                <th>وظیفه:</th>
+                                <td><?=$employee->emp_cur_place ?></td>
                             </tr>
                         </table>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h4>آدرس کارمند:</h4>
+                        <?=($employee->emp_address) ?>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4>خلص سوانح کارمند:</h4>
+                        <p><?=($employee->emp_biography) ?></p>
                     </div>
                 </div>
             </div>
