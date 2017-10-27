a80ef0c73ff7c6e13c8bec5dc43a2fa7d47b29e7
Jamshid Elmi <dariush1231@gmail.com>
2017-10-28 00:42:59 +0430

    last DB export from Home PC

--- migrations/resturant_tmp (1).sql
+++ migrations/resturant_tmp (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2017 at 09:45 PM
+-- Generation Time: Oct 27, 2017 at 10:12 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,151 +23,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Table structure for table `accounts`
 --
 
-CREATE TABLE IF NOT EXISTS `employees` (
-`emp_id` int(11) NOT NULL,
-  `emp_name` varchar(256) NOT NULL,
-  `emp_lname` varchar(256) NOT NULL,
-  `emp_position` varchar(120) NOT NULL,
-  `emp_salary` decimal(10,0) NOT NULL,
-  `emp_join_date` varchar(16) DEFAULT NULL,
-  `emp_org_place` varchar(256) NOT NULL,
-  `emp_cur_place` varchar(256) NOT NULL,
-  `emp_address` text NOT NULL,
-  `emp_email` varchar(256) DEFAULT NULL,
-  `emp_phone` char(16) NOT NULL,
-  `emp_gendar` tinyint(1) NOT NULL,
-  `emp_picture` varchar(256) NOT NULL,
-  `emp_national_id` char(16) DEFAULT NULL,
-  `emp_biography` text,
-  `emp_type` tinyint(1) NOT NULL COMMENT 'آشپزخانه یا رستورانت'
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `employees`
---
-
-INSERT INTO `employees` (`emp_id`, `emp_name`, `emp_lname`, `emp_position`, `emp_salary`, `emp_join_date`, `emp_org_place`, `emp_cur_place`, `emp_address`, `emp_email`, `emp_phone`, `emp_gendar`, `emp_picture`, `emp_national_id`, `emp_biography`, `emp_type`) VALUES
-(1, 'کریم', 'کاظمی', 'مدیر گدام', '15000', '1509988796', 'کابل', 'هرات', 'شهر نو کابل افغانستان', 'email@domain.com', '0780525265', 1, 'avatar51.png', '0255', '', 0),
-(3, 'سارا', 'شهیدی', 'سر آشپز', '15000', '1508863039', 'کابل', 'هرات', 'شهر نو کابل افغانستان', 'email@domain.com', '0780525265', 0, 'avatar2.png', '456789', '', 0),
-(6, 'محمد', 'میرزائی', 'مدیر مسئول', '15000', '1508863321', 'دایکندی ', 'کابل', 'کابل چهار قلعه وزیر آباد کوچه گل شب بو پلاک 52 نرسیده به سرک 7 بیست متری کوکاکولا.', 'email@domain.com', '078585454', 1, 'avatar04.png', '5865485', 'ویژگی های قالب و تفاوت های آن با قالب اصلی:\r\n\r\n۱- قالب به صورت کامل و حرفه ای فارسی و راست چین شده.\r\n\r\n۲- انتخاب تاریخ به صورت شمسی یا دیتا پیکر توسط کتاب خانه باباخانی اضافه شده.\r\n\r\n۳- ویرایشگر CK Editor فارسی و راست چین شده.\r\n\r\n۴- ویرایشگر TinyMCE فارسی و راست چین شده و به قالب اضافه شده.\r\n\r\n۵- همچنین فونت فارسی برای خوانایی بیشتر حروف و اعداد فارسی به قالب افزوده شد.\r\n\r\n', 0),
-(7, 'رضا', 'شایان', 'مدیر گدام', '5000', '1509375783', 'بامیان', 'کابل ', 'چهار قلعه وزیر آباد فلان سرک فلان کوچه ', 'reza@farakhaan.com', '078565475', 1, 'avatar.png', '45685', 'کارمند خوب', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jobs`
---
-
-CREATE TABLE IF NOT EXISTS `jobs` (
-`job_id` int(11) NOT NULL,
-  `job_name` varchar(256) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `jobs`
---
-
-INSERT INTO `jobs` (`job_id`, `job_name`) VALUES
-(1, 'مدیر مسئول'),
-(2, 'مدیر مالی'),
-(3, 'مدیر گدام'),
-(4, 'آشپز'),
-(5, 'سر آشپز'),
-(6, 'صفاکار'),
-(7, 'درایور');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `version` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`version`) VALUES
-(1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-`user_id` int(5) unsigned NOT NULL,
-  `user_name` varchar(256) NOT NULL,
-  `user_type` varchar(256) NOT NULL,
-  `user_pass` varchar(256) NOT NULL,
-  `emp_id` int(11) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS `accounts` (
+`acc_id` int(11) NOT NULL,
+  `acc_name` varchar(256) NOT NULL,
+  `acc_amount` decimal(10,2) NOT NULL,
+  `acc_description` varchar(512) DEFAULT NULL COMMENT 'توضیحات',
+  `acc_date` varchar(16) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `accounts`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_type`, `user_pass`, `emp_id`) VALUES
-(3, '1231', '1', '1231', 1);
+INSERT INTO `accounts` (`acc_id`, `acc_name`, `acc_amount`, `acc_description`, `acc_date`) VALUES
+(1, 'اکانت یک', '25645.00', 'توضیحات مربوط به این صندوق...', '234212'),
+(2, 'اکانت دوم', '5000.00', 'توضیحات مربوط به این صندوق...', '1324324'),
+(3, 'صندوق شماره یک', '25000.00', 'توضیحات مربوط', '1509391378');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `employees`
+-- Indexes for table `accounts`
 --
-ALTER TABLE `employees`
- ADD PRIMARY KEY (`emp_id`);
-
---
--- Indexes for table `jobs`
---
-ALTER TABLE `jobs`
- ADD PRIMARY KEY (`job_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`), ADD KEY `emp_id` (`emp_id`), ADD KEY `emp_id_2` (`emp_id`);
+ALTER TABLE `accounts`
+ ADD PRIMARY KEY (`acc_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT for table `accounts`
 --
-ALTER TABLE `employees`
-MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `jobs`
---
-ALTER TABLE `jobs`
-MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `user_id` int(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-ADD CONSTRAINT `USER_FK_EMP` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`emp_id`);
-
+ALTER TABLE `accounts`
+MODIFY `acc_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
